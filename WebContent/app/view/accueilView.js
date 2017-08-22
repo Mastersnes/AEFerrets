b7aca12907e469fa21c6dc83c4eb35af53d08314
Steven Nesztler <steven.nesztler@ac-nancy-metz.fr>
2017-08-22 15:31:05 +0200

    Fix #7 : Bug page d'accueil

--- WebContent/app/view/accueilView.js
+++ WebContent/app/view/accueilView.js
@@ -21,22 +21,11 @@ function($, _, page) {
 			this.el.html(template(templateData));
 			
 			this.makeEvents();
+			this.loadImage();
 		};
 		
 		this.makeEvents = function() {
 			var that = this;
-			$(".accueil img").on("load", function() {
-				$(".accueil img#premiere").height($(".accueil img#quatrieme").height());
-				$(".accueil img#quatrieme").height($(".accueil img#quatrieme").height());
-				$(".accueil .contenu").height($(".accueil img#quatrieme").height());
-				$(".accueil .texte").height($(".accueil img#quatrieme").height());
-				
-				$(".accueil .texte").css("margin-left", $(".accueil img#quatrieme").width()+20);
-				$(".accueil img#premiere").fadeIn("slow", function() {
-					$(".accueil img#premiere").width($(".accueil img#quatrieme").width());
-					$(".accueil img#quatrieme").show();
-				});
-			});
 			$(".accueil").mouseenter(function() {
 				if (!that.width || that.width == 0) {
 					that.width = $(".accueil #quatrieme").width();
@@ -55,6 +44,31 @@ function($, _, page) {
 			$(".accueil").on("click", function() {
 				that.parent.consult(that.parent.livres[0]);
 			});
+		};
+		
+		/**
+		 * 
+		 */
+		this.loadImage = function() {
+		    if ($(".accueil img#quatrieme").height() > 0) {
+		        $(".accueil img#quatrieme").on("load", function() {
+    		        $(".accueil img#premiere").height($(".accueil img#quatrieme").height());
+                    $(".accueil img#quatrieme").height($(".accueil img#quatrieme").height());
+                    $(".accueil .contenu").height($(".accueil img#quatrieme").height());
+                    $(".accueil .texte").height($(".accueil img#quatrieme").height());
+                    
+                    $(".accueil .texte").css("margin-left", $(".accueil img#quatrieme").width()+20);
+                    $(".accueil img#premiere").fadeIn("slow", function() {
+                        $(".accueil img#premiere").width($(".accueil img#quatrieme").width());
+                        $(".accueil img#quatrieme").show();
+                    });
+		        });
+		    }else {
+    		    var that = this;
+		        setTimeout(function() {
+    		        that.loadImage();
+    		    }, 10);
+		    }
 		};
 		
 		this.show = function() {
